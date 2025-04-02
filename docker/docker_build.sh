9785259bc9db34dd65a287c15a3b92a184a71c44
Sreeharsha <vrscar1@gmail.com>
2025-04-02 19:06:03 -0400

    Initial docker setup

---
+++ docker/docker_build.sh
@@ -0,0 +1,4 @@
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+# Replace any non-alphanumeric characters with a hyphen, making it Docker tag friendly
+MODIFIED_BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/[^a-zA-Z0-9_\-]/-/g')
+sudo docker build -f docker/Dockerfile -t boneparu:${MODIFIED_BRANCH_NAME} .
